d796113c78e634e15245ac968517a8866e47a434
Veronika-Mykhaylik <veronikamihajlik636@gmail.com>
2025-04-18 17:53:04 +0300

    Add files via upload

--- src/domain/Exercise.java
+++ src/domain/Exercise.java
@@ -1,7 +1,18 @@
 package domain;
 
 public class Exercise {
-    public static float Calculate(int R){
-        return (float) (2*Math.PI*Math.sqrt(R));
+    public static int countSymmetricTimes() {
+        int count = 0;
+        for (int h = 0; h < 24; h++) {
+            for (int m = 0; m < 60; m++) {
+                String hStr = String.format("%02d", h);
+                String mStr = String.format("%02d", m);
+                String reversedM = new StringBuilder(mStr).reverse().toString();
+                if (hStr.equals(reversedM)) {
+                    count++;
+                }
+            }
+        }
+        return count;
     }
 }
