294773880ba68e418f97ea55c5dc0f9e1cc5eff8
Veronika-Mykhaylik <veronikamihajlik636@gmail.com>
2025-04-18 17:54:19 +0300

    Add files via upload

--- src/test/TestResult.java
+++ src/test/TestResult.java
@@ -5,8 +5,7 @@ import domain.Exercise;
 public class TestResult {
 
     public static void main(String[] args) {
-
-        int R=10;
-        System.out.println("Area of a circle for R="+R+"m is "+Exercise.Calculate(R)+"m"+'\u00B2');
+        int symmetricCount = Exercise.countSymmetricTimes();
+        System.out.println("Кількість симетричних комбінацій за добу: " + symmetricCount);
     }
 }
